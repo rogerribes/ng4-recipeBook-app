f7c981f891f00261c1e6650e6f55e2841e61fc3c
Roger Ribes <Roger Ribes>
2017-05-08 02:13:53 +0200

    First visual modifications on header and recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+      new Recipe('Test Recipe','This is simply a test','https://torange.biz/photo/6/6050/6050-FX-6-0-12-6-8-0.jpg')
+  ];
 
   constructor() { }
 
